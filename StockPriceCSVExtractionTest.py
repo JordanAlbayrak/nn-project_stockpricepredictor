38c62e0afc0815e36808aaec43e19d0feefabc53
Christian Chitanu <christian.chitanu@hotmail.com>
2023-11-23 18:09:26 -0500

    got new code/model going

---
+++ StockPriceCSVExtractionTest.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import pandas as pd
+
+# Define the ticker symbol
+ticker = "AAPL"
+
+# Define the time range
+start_date = pd.to_datetime("2023-01-01")
+end_date = pd.to_datetime("2023-10-01")
+
+# Download historical stock data using yfinance
+data = yf.download(ticker, start=start_date, end=end_date)
+
+# Save the data to a CSV file
+csv_filename = f"{ticker}_daily_2023-01-01_2023-10-01_TEST.csv"
+data.to_csv(csv_filename)
+
+print(f"Stock data saved to {csv_filename}")
--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -7,7 +7,7 @@ from keras.layers import LSTM, Dense, Dropout
 import yfinance as yf
 
 # Load data
-file_path = 'TSLA_daily_2017-01-01_2023-01-01.csv'
+file_path = 'AAPL_daily_2017-01-01_2023-01-01.csv'
 df = pd.read_csv(file_path)
 df['Date'] = pd.to_datetime(df['Date'], utc=True)  # Convert the 'Date' column to datetime format
 
@@ -36,15 +36,12 @@ def create_dataset(df):
 
 
 # Train-test split
-dataset_train = np.array(prices_scaled[:int(prices_scaled.shape[0] * 0.8)])
-dataset_test = np.array(prices_scaled[int(prices_scaled.shape[0] * 0.8):])
+dataset_train = np.array(prices_scaled)
 
 x_train, y_train = create_dataset(dataset_train)
-x_test, y_test = create_dataset(dataset_test)
 
 # Reshape input for LSTM
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 # Build LSTM model
 model = Sequential()
@@ -58,16 +55,44 @@ model.add(LSTM(units=96))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(x_train, y_train, epochs=5, batch_size=32)
+hist = model.fit(x_train, y_train, epochs=21, batch_size=1, verbose=2)
+plt.plot(hist.history['loss'])
+plt.title('Training model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train'],loc='upper right')
+plt.show()
 model.save('stock_prediction.keras')
 
 # Load the trained model
 model = load_model('stock_prediction.keras')
 
 # Make predictions
+file_path = 'AAPL_daily_2023-01-01_2023-10-01_TEST.csv'
+df = pd.read_csv(file_path)
+df['Date'] = pd.to_datetime(df['Date'], utc=True)  # Convert the 'Date' column to datetime format
+
+# Extract 'Open' prices and dates
+prices = df['Open'].values
+dates = df['Date'].values
+
+# Reshape 'Open' prices
+prices = prices.reshape(-1, 1)
+
+# Normalize data
+
+print("prices",prices.shape)
+scaler = MinMaxScaler(feature_range=(0, 1))
+prices_scaled = scaler.fit_transform(prices)
+dataset_test = np.array(prices_scaled)
+x_test, y_test = create_dataset(dataset_test)
+
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+print("shape",x_test.shape)
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
-y_test_scaled = scaler.inverse_transform(y_test.reshape(-1, 1))
+print("predictions",predictions.shape)
+
 
 # Future Prediction
 x_extended = x_test[-1]
@@ -75,6 +100,7 @@ num_predictions = 30
 
 future_predictions = []
 for _ in range(num_predictions):
+    print(x_extended.shape)
     prediction = model.predict(x_extended.reshape(1, x_extended.shape[0], x_extended.shape[1]))
     future_predictions.append(prediction[0, 0])
     x_extended = np.roll(x_extended, -1)
@@ -84,7 +110,7 @@ future_predictions = np.array(future_predictions).reshape(-1, 1)
 future_predictions = scaler.inverse_transform(future_predictions)
 
 # Get the last date in the original data
-last_date = df['Date'].iloc[int(df.shape[0] * 0.8):].iloc[-1]
+last_date = df['Date'].iloc[-1]
 
 # Generate future dates starting from the next business day
 future_dates = pd.date_range(start=last_date + pd.Timedelta(days=0), periods=num_predictions, freq='B')
@@ -97,7 +123,9 @@ ax.set_facecolor('#000041')
 ax.plot(dates, prices, color='gray', label='Original prices')
 
 # Plot predicted prices
-ax.plot(dates[-len(predictions):], predictions, color='cyan', label='Predicted prices')
+print(dates.shape)
+print(predictions.shape)
+ax.plot(dates[len(dates)-predictions.shape[0]:], predictions, color='cyan', label='Predicted prices')
 
 # Plot future predictions
 ax.plot(future_dates, future_predictions, color='magenta', linestyle='dashed', label='Future predictions')
