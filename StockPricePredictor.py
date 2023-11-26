3213d606ed28da54a5cd6e695df340bc7cf50b48
Jordan Albayrak <jordan008@live.ca>
2023-11-26 15:31:51 -0500

    AI Branch Changes

--- StockPricePredictor.py
+++ StockPricePredictor.py
@@ -4,123 +4,98 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential, load_model
 from keras.layers import LSTM, Dense, Dropout
-import yfinance as yf
 
-# Load data
-file_path = 'AAPL_daily_2017-01-01_2023-01-01.csv'
-df = pd.read_csv(file_path)
-df['Date'] = pd.to_datetime(df['Date'], utc=True)  # Convert the 'Date' column to datetime format
-
-# Extract 'Open' prices and dates
-prices = df['Open'].values
-dates = df['Date'].values
-
-# Reshape 'Open' prices
-prices = prices.reshape(-1, 1)
-
-# Normalize data
-scaler = MinMaxScaler(feature_range=(0, 1))
-prices_scaled = scaler.fit_transform(prices)
+# Constants
+TRAIN_FILE_PATH = 'META_daily_2017-01-01_2023-01-01.csv'
+TEST_FILE_PATH = 'META_daily_2023-01-01_2023-10-01_TEST.csv'
+MODEL_FILE_PATH = 'stock_prediction.keras'
+WINDOW_SIZE = 50
+LSTM_UNITS = 96
+EPOCHS = 21
+BATCH_SIZE = 4
+NUM_PREDICTIONS = 50
 
 
-# Create dataset
-def create_dataset(df):
-    x = []
-    y = []
-    for i in range(50, df.shape[0]):
-        x.append(df[i - 50:i, 0])
-        y.append(df[i, 0])
-    x = np.array(x)
-    y = np.array(y)
-    return x, y
+# Function to load and preprocess data
+def load_and_preprocess_data(file_path):
+    df = pd.read_csv(file_path)
+    df['Date'] = pd.to_datetime(df['Date'], utc=True)
+    prices = df['Open'].values.reshape(-1, 1)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    prices_scaled = scaler.fit_transform(prices)
+    return df['Date'].values, prices, prices_scaled, scaler
 
 
-# Train-test split
-dataset_train = np.array(prices_scaled)
+# Function to create dataset for LSTM
+def create_lstm_dataset(data):
+    x, y = [], []
+    for i in range(WINDOW_SIZE, len(data)):
+        x.append(data[i - WINDOW_SIZE:i, 0])
+        y.append(data[i, 0])
+    return np.array(x), np.array(y)
 
-x_train, y_train = create_dataset(dataset_train)
 
-# Reshape input for LSTM
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+# Function to build LSTM model
+def build_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(units=LSTM_UNITS, return_sequences=True, input_shape=input_shape))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=LSTM_UNITS, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=LSTM_UNITS, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=LSTM_UNITS))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
 
-# Build LSTM model
-model = Sequential()
-model.add(LSTM(units=96, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96, return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=96))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-hist = model.fit(x_train, y_train, epochs=70, batch_size=32, verbose=2)
-plt.plot(hist.history['loss'])
-plt.title('Training model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train'],loc='upper right')
+
+# Load and preprocess training data
+dates_train, prices_train, prices_scaled_train, scaler = load_and_preprocess_data(TRAIN_FILE_PATH)
+x_train, y_train = create_lstm_dataset(prices_scaled_train)
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
+
+# Build and train the model
+model = build_lstm_model((WINDOW_SIZE, 1))
+history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=2)
+model.save(MODEL_FILE_PATH)
+
+# Plot training loss
+plt.plot(history.history['loss'])
+plt.title('Training Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train'], loc='upper right')
 plt.show()
-model.save('stock_prediction.keras')
 
-# Load the trained model
-model = load_model('stock_prediction.keras')
+# Load and preprocess test data
+dates_test, prices_test, prices_scaled_test, _ = load_and_preprocess_data(TEST_FILE_PATH)
+x_test, _ = create_lstm_dataset(prices_scaled_test)
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
 
-# Make predictions
-file_path = 'AAPL_daily_2023-01-01_2023-10-01_TEST.csv'
-df = pd.read_csv(file_path)
-df['Date'] = pd.to_datetime(df['Date'], utc=True)  # Convert the 'Date' column to datetime format
-
-# Extract 'Open' prices and dates
-prices = df['Open'].values
-dates = df['Date'].values
-
-# Reshape 'Open' prices
-prices = prices.reshape(-1, 1)
-
-# Normalize data
-scaler = MinMaxScaler(feature_range=(0, 1))
-prices_scaled = scaler.fit_transform(prices)
-dataset_test = np.array(prices_scaled)
-x_test, y_test = create_dataset(dataset_test)
-
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+# Load the model and make predictions
+model = load_model(MODEL_FILE_PATH)
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 
-# Future Prediction
-x_extended = x_test[-1]
-num_predictions = 50
-
+# Future predictions
+x_extended = np.copy(x_test[-1])
 future_predictions = []
-for _ in range(num_predictions):
-    prediction = model.predict(x_extended.reshape(1, x_extended.shape[0], x_extended.shape[1]))
-    future_predictions.append(prediction[0, 0])
+for _ in range(NUM_PREDICTIONS):
+    prediction = model.predict(x_extended.reshape(1, WINDOW_SIZE, 1))[0, 0]
+    future_predictions.append(prediction)
     x_extended = np.roll(x_extended, -1)
-    x_extended[-1] = prediction[0, 0]
-
+    x_extended[-1] = prediction
 future_predictions = np.array(future_predictions).reshape(-1, 1)
 future_predictions = scaler.inverse_transform(future_predictions)
 
-# Get the last date in the original data
-last_date = df['Date'].iloc[-1]
-
-# Generate future dates starting from the next business day
-future_dates = pd.date_range(start=last_date + pd.Timedelta(days=0), periods=num_predictions, freq='B')
-
-# Plot the entire dataset
+# Plot results
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.set_facecolor('#000041')
-
-# Plot original prices
-ax.plot(dates, prices, color='gray', label='Original prices')
-
-# Plot predicted prices
-ax.plot(dates[len(dates)-predictions.shape[0]:], predictions, color='cyan', label='Predicted prices')
-
-# Plot future predictions
-ax.plot(future_dates, future_predictions, color='magenta', linestyle='dashed', label='Future predictions')
-
+ax.plot(dates_test, prices_test, color='gray', label='Original Prices')
+ax.plot(dates_test[len(dates_test) - len(predictions):], predictions, color='cyan', label='Predicted Prices')
+future_dates = pd.date_range(start=dates_test[-1], periods=NUM_PREDICTIONS, freq='B')
+ax.plot(future_dates, future_predictions, color='magenta', linestyle='dashed', label='Future Predictions')
 plt.legend()
 plt.show()
